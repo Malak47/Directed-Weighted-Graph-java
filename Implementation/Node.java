516587c468e35aba4b453f9acfb3ca531595462d
Malak47 <malak.qeedan@hotmail.com>
2021-12-06 19:06:00 +0200

    Node Class (Not Finished)!

--- Implementation/Node.java
+++ Implementation/Node.java
@@ -1,4 +1,58 @@
 package api.Implementation;
 
-public class Node {
+import api.api.GeoLocation;
+import api.api.NodeData;
+
+public class Node implements NodeData {
+    Integer key;
+    GeoLocation location;
+    double weight;
+    String info;
+    int tag;
+
+
+    @Override
+    public int getKey() {
+        return this.key;
+    }
+
+    @Override
+    public GeoLocation getLocation() {
+        return this.location;
+    }
+
+    @Override
+    public void setLocation(GeoLocation p) {
+        this.location = p;
+    }
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public void setWeight(double w) {
+        this.weight = w;
+    }
+
+    @Override
+    public String getInfo() {
+        return this.info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+        this.info = s;
+    }
+
+    @Override
+    public int getTag() {
+        return this.tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+        this.tag = t;
+    }
 }

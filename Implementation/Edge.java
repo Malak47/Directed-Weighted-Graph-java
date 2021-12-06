f4c8b6a468b02a8347c2c648b31f1a2b9049c679
Malak47 <malak.qeedan@hotmail.com>
2021-12-06 21:36:29 +0200

    Edge, GeoL (Not Finished)!

---
+++ Implementation/Edge.java
@@ -0,0 +1,46 @@
+package api.Implementation;
+
+import api.api.EdgeData;
+
+public class Edge implements EdgeData {
+    int src;
+    int dest;
+    double weight;
+    String info;
+    int tag;
+
+    @Override
+    public int getSrc() {
+        return this.src;
+    }
+
+    @Override
+    public int getDest() {
+        return this.dest;
+    }
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public String getInfo() {
+        return this.info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+        this.info = s;
+    }
+
+    @Override
+    public int getTag() {
+        return this.tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+        this.tag = t;
+    }
+}
---
+++ Implementation/GeoL.java
@@ -0,0 +1,37 @@
+package api.Implementation;
+
+import api.api.GeoLocation;
+
+public class GeoL implements GeoLocation {
+    double _x;
+    double _y;
+    double _z;
+
+    public GeoL(double x, double y, double z){
+        this._x = x;
+        this._y = y;
+        this._z = z;
+    }
+    @Override
+    public double x() {
+        return _x;
+    }
+
+    @Override
+    public double y() {
+        return _y;
+    }
+
+    @Override
+    public double z() {
+        return _z;
+    }
+
+    @Override
+    public double distance(GeoLocation g) {
+        double d1 = Math.pow(this._x - g.x(), 2);
+        double d2 = Math.pow(this._y - g.y(), 2);
+        double d3 = Math.pow(this._z - g.z(), 2);
+        return Math.sqrt(d1 + d2 + d3);
+    }
+}

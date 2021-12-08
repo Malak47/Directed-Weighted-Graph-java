1cb67789c6d0cfc97528fbb7736bfb7f9c965e88
Malak47 <malak.qeedan@hotmail.com>
2021-12-08 14:52:24 +0200

    Creating tests

---
+++ test/src/NodeTest.java
@@ -0,0 +1,66 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void getKey() {
+    }
+
+    @Test
+    void getLocation() {
+    }
+
+    @Test
+    void setLocation() {
+    }
+
+    @Test
+    void getWeight() {
+    }
+
+    @Test
+    void setWeight() {
+    }
+
+    @Test
+    void getInfo() {
+    }
+
+    @Test
+    void setInfo() {
+    }
+
+    @Test
+    void getTag() {
+    }
+
+    @Test
+    void setTag() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void addEdge() {
+    }
+
+    @Test
+    void addEdgeOut() {
+    }
+
+    @Test
+    void getEdge() {
+    }
+
+    @Test
+    void getAllEdgesIn() {
+    }
+
+    @Test
+    void getAllEdgesOut() {
+    }
+}
